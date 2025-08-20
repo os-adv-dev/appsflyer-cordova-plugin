67869ef373a90723715dfad2b1552c4150e63813
pcamilojunior <paulo.camilo.junior@outsystems.com>
2025-08-20 16:51:58 +0200

    set hook to check if adobe is installed to register our own FCM message

---
+++ src/android/com/appsflyer/cordova/plugin/AppsFlyerFirebaseMessagingService.java
@@ -0,0 +1,29 @@
+package com.appsflyer.cordova.plugin;
+
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.appsflyer.AppsFlyerLib;
+import com.google.firebase.messaging.RemoteMessage;
+import com.outsystems.plugins.firebasemessaging.controller.FirebaseMessagingReceiveService;
+
+public class AppsFlyerFirebaseMessagingService extends FirebaseMessagingReceiveService {
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        AppsFlyerLib.getInstance().updateServerUninstallToken(getApplicationContext(), token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if(remoteMessage.getData().containsKey("af-uinstall-tracking") || remoteMessage.getData().containsKey("af-uninstall-tracking")){ // "uinstall" is not a typo
+            Log.v(AppsFlyerFirebaseMessagingService.class.getSimpleName(), "onMessageReceived: af-uinstall-tracking");
+            return; // "uinstall" is not a typo
+        } else {
+            super.onMessageReceived(remoteMessage);
+        }
+    }
+}
